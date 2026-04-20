848e1fea323224d6b8bc821344e274afaaeb6225
ibbitiminecraft <khadijakanwal90@gmail.com>
2026-04-19 20:30:35 -0500

    dcdscs

--- geometry_dash/Assets/scripts/player script/player.cs
+++ geometry_dash/Assets/scripts/player script/player.cs
@@ -1,20 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class player : MonoBehaviour
 {
     private float motion_force;
     [SerializeField]
+    private float jump_force;
     private Transform transform;
+    private Rigidbody2D myBody;
+    private bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform = GetComponent<Transform>();
         motion_force = 10;
+        myBody = GetComponent<Rigidbody2D>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += new Vector3(1, 0, 0) * motion_force * Time.deltaTime;
+        Jump();
+    }
+    void Jump()
+    {
+        if (Keyboard.current.upArrowKey.isPressed && isGrounded == true)
+        {
+            myBody.AddForce(new Vector2(0f, jump_force), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
